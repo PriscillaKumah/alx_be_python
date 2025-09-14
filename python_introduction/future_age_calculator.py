c3a80730278fd534f39ecd09167a68d3b5c00a02
Priscilla Kumah <prissydzifa653@gmail.com>
2025-09-14 20:09:34 +0000

    A program to compute future age of a user

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+#A python script to calculate the age of a person inthe next five years
+#prompt user for their current age
+
+current_age = int(input("How old are you? "))
+
+# Calculate age 
+future_age = current_age + 27
+
+# Print the result
+print(f"In 2050, you will be {future_age} years old.")
