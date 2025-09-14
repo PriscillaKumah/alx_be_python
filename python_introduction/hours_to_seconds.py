5ceffaab37085b0ea516da9506632e26011b1140
Priscilla Kumah <prissydzifa653@gmail.com>
2025-09-14 19:38:31 +0000

    compute hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+#A program to convert time from hours to seconds
+#Declare a variable and assign values 
+hours = 2
+seconds = hours * 3600
+
+#Output
+print(f"{hours} hour(s) is {seconds} seconds.")
+print("{hours} hour(s) is {seconds} seconds.")
+
