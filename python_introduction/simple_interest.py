e9a43d4674a7d04cae832051befe1c2d8bd87da7
Priscilla Kumah <prissydzifa653@gmail.com>
2025-09-14 18:11:17 +0000

    compute simple interest over time

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,17 @@
+#A program to compute simple interest earned on an investment over a period of time.
+
+#Declare Variables and assign values
+principal = 1000
+rate = 0.05
+time = 3
+
+#Simple Interest formula(Interest= Principal * Rate * Time)
+#let interest be the variable
+
+interest = principal * rate * time
+print(f"principal: {principal}")
+print(f"rate: {rate}")
+print(f"time: {time}")
+
+#Output
+print("The simple interest is", [interest])
